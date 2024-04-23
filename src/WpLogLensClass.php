8d60aea415fabe9efec40182d8c0e33ab6fe3f21
imerfanahmed <imerfanahmed@users.noreply.github.com>
2024-04-23 14:22:11 +0000

    Fix styling

--- src/WpLogLensClass.php
+++ src/WpLogLensClass.php
@@ -2,42 +2,44 @@
 
 namespace Imerfanahmed\WpLogLens;
 
-final class WPLogLensClass{
-
+final class WPLogLensClass
+{
     private $patterns = [
         'logs' => '/\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}([\+-]\d{4})?\].*/',
         'current_log' => [
             '/^\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}([\+-]\d{4})?)\](?:.*?(\w+)\.|.*?)',
-            ': (.*?)( in .*?:[0-9]+)?$/i'
+            ': (.*?)( in .*?:[0-9]+)?$/i',
         ],
         'files' => '/\{.*?\,.*?\}/i',
     ];
-    private function __construct() {
+
+    private function __construct()
+    {
         $requestUri = $_SERVER['REQUEST_URI'];
 
         //reading logs file from wp-content
-        $logs = fopen(ABSPATH . 'wp-content/debug.log', 'r');
-        $logData = fread($logs, filesize(ABSPATH . 'wp-content/debug.log'));
+        $logs = fopen(ABSPATH.'wp-content/debug.log', 'r');
+        $logData = fread($logs, filesize(ABSPATH.'wp-content/debug.log'));
         fclose($logs);
 
         //match the logs data with patterns
         preg_match_all($this->patterns['logs'], $logData, $matches);
         ray($matches);
 
-
-        if( $requestUri === '/logs' ) {
+        if ($requestUri === '/logs') {
             ob_start();
-            require_once __DIR__ . '/views/index.php';
+            require_once __DIR__.'/views/index.php';
             $content = ob_get_clean();
             var_dump($logData);
-            die();
+            exit();
         }
     }
 
-    public static function init() {
+    public static function init()
+    {
         static $instance = false;
 
-        if ( ! $instance ) {
+        if (! $instance) {
             $instance = new self();
         }
 
--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,8 @@
 <?php
 
-
 return [
     'GET' => [
-        '/logs'   => 'Controller1@method1',
+        '/logs' => 'Controller1@method1',
         '/route2' => 'Controller2@method2',
     ],
 ];
