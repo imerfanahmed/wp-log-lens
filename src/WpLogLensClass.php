b4c733867f976661314317bd22231b2adbbdf519
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2024-04-23 17:12:07 +0600

    WIP

--- src/WpLogLensClass.php
+++ src/WpLogLensClass.php
@@ -2,11 +2,33 @@
 
 namespace Imerfanahmed\WpLogLens;
 
-class WpLogLensClass
-{
-    public function __construct()
-    {
-        add_action('admin_menu', [$this, 'addAdminMenu']);
+final class WPLogLensClass{
+
+    private function __construct() {
+        $requestUri = $_SERVER['REQUEST_URI'];
+
+        //reading logs file from wp-content
+        $logs = fopen(ABSPATH . 'wp-content/debug.log', 'r');
+        $logData = fread($logs, filesize(ABSPATH . 'wp-content/debug.log'));
+        fclose($logs);
+
+
+        if( $requestUri === '/logs' ) {
+            ob_start();
+            require_once __DIR__ . '/views/index.php';
+            $content = ob_get_clean();
+            var_dump($logData);
+            die();
+        }
     }
 
+    public static function init() {
+        static $instance = false;
+
+        if ( ! $instance ) {
+            $instance = new self();
+        }
+
+        return $instance;
+    }
 }
---
+++ src/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+
+return [
+    'GET' => [
+        '/logs'   => 'Controller1@method1',
+        '/route2' => 'Controller2@method2',
+    ],
+];
